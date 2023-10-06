7869659033513d33b6a0011209472082416d632a
Agroall <110466247+Agroall@users.noreply.github.com>
2023-10-06 21:31:04 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,7 +41,10 @@ def app():
     
     # Add a dropdown to select the line plot
     plot_choice = st.selectbox('Select line plot', [
-        'Number of Posts Made on All Media Platforms'
+        'Number of Posts Made on All Media Platforms',
+        'Average Monthly Impressions Made on All Media',
+        'Average Monthly Engagements Made on All Media',
+        'Average Monthly Engagements Rate Per Impression Made on All Media'
   
     ])
     
@@ -69,6 +72,81 @@ def app():
         ax.set_title('Time Series Analysis of The Number of Posts Made on All Media Platforms')
         ax.legend()
         plt.grid(which='major',axis='y')
+
+    elif plot_choice == 'Average Monthly Impressions Made on All Media':
+        fig, ax = plt.subplots(figsize=[20, 10])
+        start_date = min(media['year_month'].min() for media in mediums_impression_temp)
+        end_date = max(media['year_month'].max() for media in mediums_impression_temp)
+        date_range = pd.date_range(start=start_date, end=end_date, freq='M')
+        for index, media in enumerate(mediums_impression_temp):
+            impressions_avg_temp = media.groupby('year_month')['Impressions'].mean()
+            df={'impressions_avg_temp':impressions_avg_temp}
+            impressions_avg_temp=pd.DataFrame(df, index=impressions_avg_temp.index)
+            impressions_avg_temp['date']=impressions_avg_temp.index
+            sns.lineplot(
+                data=impressions_avg_temp,
+                x='date',
+                y='impressions_avg_temp',
+                ax=ax,
+                label=mediums_list[index],
+                linewidth=3
+            )
+        ax.set_xticks(range(0, len(date_range), 3))
+        ax.set_xticklabels(date_range[::3].strftime('%Y-%m'), rotation=90, fontsize=12)
+        ax.set_title('Time Series Analysis of The Average Monthly Impressions Made on All Media')
+        ax.legend()
+        plt.grid(which='major',axis='y')
+    elif plot_choice == 'Average Monthly Engagements Made on All Media':
+        fig, ax = plt.subplots(figsize=[20, 10])
+        start_date = min(media['year_month'].min() for media in mediums_engagement_temp)
+        end_date = max(media['year_month'].max() for media in mediums_engagement_temp)
+        date_range = pd.date_range(start=start_date, end=end_date, freq='M')
+        for index, media in enumerate(mediums_engagement_temp):
+            engagement_avg_temp = media.groupby('year_month')['Engagements'].mean()
+            engagement_avg_temp[engagement_avg_temp > 1500] = 1200
+            df={'engagement_avg_temp':engagement_avg_temp}
+            engagement_avg_temp=pd.DataFrame(df, index=engagement_avg_temp.index)
+            engagement_avg_temp['date']=engagement_avg_temp.index
+            
+            sns.lineplot(
+                data=engagement_avg_temp,
+                x='date',
+                y='engagement_avg_temp',
+                ax=ax,
+                label=mediums_list[index],
+                linewidth=3
+            )
+        ax.set_xticks(range(0, len(date_range), 3))
+        ax.set_xticklabels(date_range[::3].strftime('%Y-%m'), rotation=90, fontsize=12)
+        plt.grid(which='major',axis='y')
+        ax.set_title('Time Series Analysis of The Average Monthly Engagements Made on All Media')
+        ax.legend()
+    elif plot_choice == 'Average Monthly Engagements Rate Per Impression Made on All Media':
+        fig, ax = plt.subplots(figsize=[20, 10])
+        start_date = min(media['year_month'].min() for media in mediums)
+        end_date = max(media['year_month'].max() for media in mediums)
+        date_range = pd.date_range(start=start_date, end=end_date, freq='M')
+        for index, media in enumerate(mediums_engagement_temp):
+            engagement_avg_temp = media.groupby('year_month')['Engagement Rate (per Impression)'].mean()
+            engagement_avg_temp[engagement_avg_temp > 25] = 25
+            df={'engagement_avg_temp':engagement_avg_temp}
+            engagement_avg_temp=pd.DataFrame(df, index=engagement_avg_temp.index)
+            engagement_avg_temp['date']=engagement_avg_temp.index
+            
+            sns.lineplot(
+                data=engagement_avg_temp,
+                x='date',
+                y='engagement_avg_temp',
+                ax=ax,
+                label=mediums_list[index],
+                linewidth=3
+            )
+            
+        ax.set_xticks(range(0, len(date_range), 3))
+        ax.set_xticklabels(date_range[::3].strftime('%Y-%m'), rotation=90, fontsize=12)
+        plt.grid(which='major',axis='y')
+        ax.set_title('Time Series Analysis of The Average Monthly Engagements Rate Per Impression Made on All Media')
+        ax.legend()
     
     
     # Display the selected line plot
